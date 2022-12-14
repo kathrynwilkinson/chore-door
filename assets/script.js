f714e4b178d129a6768ede0992f614708a761e69
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-12-13 19:08:44 -0500

    Define game logic to check doors, progress game, end game, and choose a random chore door

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,32 @@ let openDoor1;
 let openDoor2;
 let openDoor3;
 let currentlyPlaying = true;
+
+// Define game logic to check doors, progress game, end game, and choose a random chore door
+
+doorImage1.onclick = () => {
+	if (currentlyPlaying && isClicked(doorImage1)) {
+	  doorImage1.src = openDoor1;
+	  playDoor(doorImage1);
+	}
+}
+
+doorImage2.onclick = () => {
+	if (currentlyPlaying && isClicked(doorImage2)) {
+		doorImage2.src = openDoor2;
+		playDoor(doorImage2);
+	}
+}
+
+doorImage3.onclick = () => {
+	if (currentlyPlaying && isClicked(doorImage3)) {
+		doorImage3.src = openDoor3;
+		playDoor(doorImage3);
+	}
+}
+
+startButton.onclick = () => {
+	if (currentlyPlaying === false) {
+		startRound();
+	}
+}
