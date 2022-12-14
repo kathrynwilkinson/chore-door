e382bcd99c1e789a102bea52a4771643eca23dfa
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2022-12-13 19:06:02 -0500

    Add script file; Access HTML elements

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// Access HTML elements
+
+let botDoorPath = 'https://content.codecademy.com/projects/chore-door/images/robot.svg';
+let beachDoorPath = 'https://content.codecademy.com/projects/chore-door/images/beach.svg';
+let spaceDoorPath = 'https://content.codecademy.com/projects/chore-door/images/space.svg';
+let closedDoorPath = 'https://content.codecademy.com/projects/chore-door/images/closed_door.svg';
+
+let numClosedDoors = 3;
+let openDoor1;
+let openDoor2;
+let openDoor3;
+let currentlyPlaying = true;
